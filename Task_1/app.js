5ad40c6c3918c8a373ec32aa0c0c767d72a90ff9
Shashank Valugunuri <04shashank26v@gmail.com>
2021-12-08 20:30:06 +0530

    new files added

--- Task_1/app.js
+++ Task_1/app.js
@@ -42,3 +42,42 @@ $(document).ready(function () {
     ]
   });
 })
+
+$(document).ready(function () {
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+})
+})
+
+$(document).ready(function () {
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  // asNavFor: '.slider-for',
+  autoplay: true,
+  dots: false,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+  centerMode: true,
+  focusOnSelect: true,
+  centerPadding: 0,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    }]
+})
+})
+
+function changeImage(image){
+  document.getElementById('main-img').src = image;
+}
